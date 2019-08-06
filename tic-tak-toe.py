e10a103df89bf335c1ac9ee10b38ee5507dfcfaa
shawn <shawnantonucci2018@gmail.com>
2019-08-05 23:06:48 -0700

    game_board function

--- tic-tak-toe.py
+++ tic-tak-toe.py
@@ -2,9 +2,15 @@ game = [[0, 0, 0],
               [0, 0, 0],
               [0, 0, 0]]
 
-print("   0  1  2")
-count = 0
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    print("   0  1  2")
+    if not just_display:
+        game_map[row][column] = player
+    for count, row in enumerate(game_map):
+        print(count, row)
 
-for row in game:
-    print(count, row)
-    count += 1
+    return game_map
+
+
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=1)
